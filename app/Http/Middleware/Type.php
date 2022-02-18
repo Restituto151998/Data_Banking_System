a08b0a8f55b07a2585d6bbdce1fa834ee8a9e887
Restituto151998 <restitutofuentes0@gmail.com>
2022-02-18 22:25:07 +0800

    LogIn_Auth

---
+++ app/Http/Middleware/Type.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Auth\Middleware\Type as Middleware;
+use Illuminate\Support\Facades\Auth;
+
+class Type {
+
+  public function handle($request, Closure $next, String $type) {
+    if (!Auth::check()) // This isnt necessary, it should be part of your 'auth' middleware
+      return redirect('/home');
+
+    $user = Auth::user();
+    if($user->type == $type)
+      return $next($request);
+
+    return redirect('/home');
+  }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * 
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate(); //for cleaning earlier data to avoid duplicate entries
+        DB::table('users')->insert([
+    'name' => 'admin',
+    'email' => 'admin@admin',
+    'type' => 'ADMIN',
+    'password' => Hash::make('password'),
+  ]);
+        DB::table('users')->insert([
+    'name' => 'staff',
+    'email' => 'staff@staff',
+    'type' => 'STAFF',
+    'password' => Hash::make('password'),
+  ]);
+    }
+}
