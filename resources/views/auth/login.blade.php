0fe3a41cd2edfe52f6cd41ce28fe9f18ec98adcf
capstone student <capstonestudent@pn.local>
2022-02-23 20:19:03 +0800

    alex

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,17 +6,16 @@
         <div class="container mt-5">
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                    <div class="card card-primary">
+                    <div class="card card-success">
                         <div class="card-header">
-                            <h4>Login</h4>
+                            <h4>Welcome Back!</h4>
                         </div>
                         <div class="card-body">
 
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
                                 <div class="form-group">
-                                    <label for="email">Email</label>
-
+                                    <label for="email">Username:</label>
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                                         name="email" tabindex="1" value="{{ old('email') }}" required autocomplete="email"
                                         autofocus>
@@ -27,7 +26,7 @@
                                     @enderror
                                 </div>
                                 <div class="form-group mt-3">
-                                    <label for="email">Password</label>
+                                    <label for="email">Password:</label>
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         tabindex="2" required autocomplete="current-password">
@@ -38,10 +37,13 @@
                                     @enderror
                                 </div>
                                 <div class="form-group mt-3">
-                                    <button type="submit" tabindex="4" class="btn btn-primary btn-lg btn-block">
+                                    <button type="submit" tabindex="4" class="btn btn-success btn-lg btn-block">
                                         {{ __('Login') }}
                                     </button>
                                 </div>
+                                <div class="card-footer">
+                            <p>Forgot Password? click here</p>
+                        </div>
                             </form>
                         </div>
                     </div>
