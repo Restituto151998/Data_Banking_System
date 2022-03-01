a2a2810f1890bf7732b4a150a2fca185fd7acf70
capstone student <capstonestudent@pn.local>
2022-03-01 22:07:11 +0800

    Profile and add resort changes

--- resources/views/admin/add_resort.blade.php
+++ resources/views/admin/add_resort.blade.php
@@ -1,73 +1,75 @@
 @extends('sideNav.side_navbar')
 
 @section('addResort')
-    <div>
-        <div class="main-wrapper main-wrapper-1">
-            <!-- Main Content -->
-            <div class="main-content">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card mb-0">
-                            <div class="card-body">
-                                <div class="row mt-4">
-                                    <div class="col-10">
-                                            <div class="card-body">
-                                                <div class="add-resort-header">
-                                                    <h2>Add Resort</h2>
-                                                </div>
-                                                @if (session('status'))
-                                                    <div class="alert alert-success">
-                                                        {{ session('status') }}
-                                                    </div>
-                                                @endif
-                                                <div class="card-body">
-                                                    <form method="post" enctype="multipart/form-data" id="add_resort"
-                                                        action="{{ url('add_resort') }}">
-                                                        @csrf
-                                                        <div class="row">
-
-                                                            <div class="col-md-12">
-                                                                <div class="form-group">
-                                                                           
-                                                            <img id="preview-image-before-upload" src="/assets/img/no_image.png"
-                                                            alt="preview image" style="max-height: 250px;">
-                                                                </div>
-                                                            </div>
-                                                
-                                                            <div class="col-md-12">
-                                                                <div class="form-group">
-                                                                    <input type="file" name="image"
-                                                                        placeholder="Choose image" id="image">
-                                                                    @error('image')
-                                                                        <div class="alert alert-danger mt-1 mb-1">
-                                                                            {{ $message }}</div>
-                                                                    @enderror
-                                                                </div>
-                                                            </div>
-                                                          
-                                                            <div class="row mt-5">
-                                                                <div class="form-group">
-                                                                    <label for="formGroupExampleInput">Description</label>
-                                                                    <textarea name="resort_description" class="form-control"
-                                                                        id="exampleFormControlTextarea1"
-                                                                        rows="3"></textarea>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-12 mt-5">
-                                                                <button type="submit" class="btn btn-primary"
-                                                                    >Submit</button>
+        <div>
+            <div class="main-wrapper main-wrapper-1">
+                <!-- Main Content -->
+                <div class="main-content" >
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card mb-0">
+                                <div class="card-body">
+                                    <form method="post" enctype="multipart/form-data" id="add_resort"
+                                    action="{{ url('add_resort') }}">
+                                    @csrf
+                                    <div class="row mt-2">
+                                        <div class="col-5">
+                                            <div class="row">
+                                                <div class="col text-center">
+                                                    <div class="form-group">
+                                                        <img id="preview-image-before-upload"
+                                                        src="/assets/img/no_image.png" alt="preview image"
+                                                        style="max-height: 250px;">
+                                                        <div class="col">
+                                                            <div class="form-group">
+                                                                <input type="file" name="image" placeholder="Choose image" 
+                                                                    id="image" >
+                                                                @error('image')
+                                                                    <div class="alert alert-danger mt-1 mb-1">
+                                                                        {{ $message }}</div>
+                                                                @enderror
                                                             </div>
                                                         </div>
-                                                    </form>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
+                                        <div class="col-7">
+                                                    <h4 style="text-align: center; color:black">Add Resort</h4>
+                                                    <div class="card-body">
+                                                    </div>
+                                                    @if (session('status'))
+                                                        <div class="alert alert-success">
+                                                            {{ session('status') }}
+                                                        </div>
+                                                        @endif
+                                                <div class="card-body">
+                                                        <div class="form-group">
+                                                            <label for="resortName">Resort Name:</label>
+                                                            <input type="name" class="form-control" id="resort name" placeholder="Enter resort name">
+                                                          </div>
+                                                        <div class="row">
+                                                            <div class="form-group">
+                                                                <label for="formGroupExampleInput">Description:</label>
+                                                                <textarea name="resort_description" class="form-control"
+                                                                    id="exampleFormControlTextarea1" rows="3" placeholder="Enter resort description"></textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div class="row ">
+                                                            <div class="form-group">
+                                                                <button class="btn w-25"
+                                                                style="background-color:#21791A; color:white">Add</button>
+                                                            </div>
+                                                        </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+                        {{-- dfgfdgdfgfd --}}
 @endsection
--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -1,25 +1,22 @@
 @extends('sideNav.side_navbar')
 
 @section('profile')
-    <<div>
-        <div class="main-wrapper main-wrapper-1"> -->
+    <div>
+        <div class="main-wrapper main-wrapper-1">
             <!-- Main Content -->
-            <div class="main-content">
+            <div class="main-content" >
                 <div class="row">
                     <div class="col-12">
                         <div class="card mb-0">
                             <div class="card-body">
-
-                                <div class="row mt-4">
+                                <div class="row mt-2">
                                     <div class="col-3">
                                         <div class="row">
-                                            <div class="col-md-12 text-center">
+                                            <div class="col text-center">
                                                 <div class="form-group">
-
                                                     <img id="preview-profile-image" src="/assets/img/default_profile.jpg"
                                                         alt="preview image" style="width:200px; height: 200px;"
                                                         class="rounded-circle">
-
                                                     <label for="profile">
                                                         <i data-feather="camera"
                                                             style="margin-left:-50px; margin-top:90px; position: absolute"></i>
@@ -30,68 +27,50 @@
                                                         <div class="alert alert-danger mt-1 mb-1">
                                                             {{ $message }}</div>
                                                     @enderror
-
-
                                                 </div>
                                                 <div class="form-group">
-                                                    <button class="btn btn-primary text-center" id="change_profile" disabled>Change Profile</button>
+                                                    <button class="btn text-center" style="background-color:#21791A; color:white" id="change_profile" disabled>Change Profile</button>
                                                 </div>
                                             </div>
-
-
-
-
                                         </div>
                                     </div>
                                     <div class="col-9">
-                                        <div class="card card-primary">
-                                            <div class="card-header">
-                                                <h4>Profile Info</h4>
-                                            </div>
+                                                <h4 style="text-align: center; color:black">Profile Information</h4>
                                             <div class="card-body">
                                                 <center>
-                                                <div class="row ">
-                                                    <div class="form-group col-6 text-center">
-                                                   
-                                                    <span style="font-size: 30px">{{ Auth::user()->name }}</span><hr>
+                                                    <div class="row ">
+                                                        <div class="form-group col-6 text-center">
+                                                            <span style="font-size: 20px;color:black">{{ Auth::user()->name }}</span>
+                                                            <hr style="color: #21791A">
                                                             <label for="name">Name</label>
-
+                                                        </div>
+                                                        <div class="form-group col-6 ">
+                                                            <span style="font-size: 20px; color:black">{{ Auth::user()->email }}</span>
+                                                            <hr style="color: #21791A">
+                                                            <label class="small mb-1" for="name">Email Address</label>
+                                                        </div>
                                                     </div>
-                                                    <div class="form-group col-6 ">
-                                                  
-                                                        <span style="font-size: 30px">{{ Auth::user()->email }}</span><hr>
-                                                    
-                                                            <label for="name">Email Address</label>
-
-                                                    </div>
-                                                </div>
-                                                <div class="row ">
-                                          
-                                                    <div class="form-group">
-                                                      
-                                                        <span style="font-size: 15px">{{ Auth::user()->password }}</span><hr>
-                                                          
+                                                    <div class="row">
+                                                        <div class="form-group">
+                                                            <span
+                                                                style="font-size: 15px; color:black">{{ Auth::user()->password }}</span>
+                                                            <hr style="color: #21791A">
                                                             <label for="password">Password</label>
+                                                        </div>
                                                     </div>
-                                               
-                                                </div>
-                                                <div class="row ">
-                                          
-                                                    <div class="form-group">
-                                                      
-                                                        <button class="btn btn-primary w-50">Edit Info</button>
+                                                    <div class="row ">
+                                                        <div class="form-group">
+                                                            <button class="btn w-50" style="background-color:#21791A; color:white">Edit Information</button>
+                                                        </div>
                                                     </div>
-                                               
-                                                </div>
-                                            </center>
+                                                </center>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
                     </div>
                 </div>
             </div>
-        </div> 
+        </div>
     @endsection
