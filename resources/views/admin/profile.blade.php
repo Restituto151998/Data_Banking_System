f324b4faefcd50526449fe0556dfcbcf8ae15e5c
capstone student <capstonestudent@pn.local>
2022-03-02 20:22:41 +0800

    OKNA

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -4,20 +4,16 @@
     <div>
         <div class="main-wrapper main-wrapper-1">
             <!-- Main Content -->
-<<<<<<< HEAD
             <div class="main-content">
                 @if (session()->has('status'))
-                    <div class="alert alert-success alert-dismissible fade  show" role="alert">
-                        {{ session()->get('status') }}
+                <div class="alert alert-success alert-dismissible fade  show" role="alert">
+                    {{ session()->get('status') }}
 
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                @endif
-
-            <div class="main-content" >
-
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
                 <div class="row">
                     <div class="col-12">
                         <div class="card mb-0">
@@ -25,8 +21,7 @@
                                 <div class="row mt-2">
                                     <div class="col-3">
                                         <div class="row">
-<<<<<<< HEAD
-                                            <div class="col-md-12 text-center">
+                                            <div class="col text-center">
 
                                                 <form action="{{ url('/profile') }}" method="POST"
                                                     enctype="multipart/form-data">
@@ -52,112 +47,54 @@
 
                                                     </div>
                                                     <div class="form-group">
-                                                        <button type="submit" class="btn btn-primary text-center"
+                                                        <button type="submit" class="btn  text-center text-white" style="background-color:  #21791A"
                                                             id="change_profile" disabled>Change Profile</button>
                                                     </div>
-
+                                                </form>
                                             </div>
-                                            </form>
 
-
-
-
-=======
-                                            <div class="col text-center">
-                                                <div class="form-group">
-                                                    <img id="preview-profile-image" src="/assets/img/default_profile.jpg"
-                                                        alt="preview image" style="width:200px; height: 200px;"
-                                                        class="rounded-circle">
-                                                    <label for="profile">
-                                                        <i data-feather="camera"
-                                                            style="margin-left:-50px; margin-top:90px; position: absolute"></i>
-                                                    </label>
-                                                    <input type="file" name="image" placeholder="Choose image" id="profile"
-                                                        hidden>
-                                                    @error('image')
-                                                        <div class="alert alert-danger mt-1 mb-1">
-                                                            {{ $message }}</div>
-                                                    @enderror
-                                                </div>
-                                                <div class="form-group">
-                                                    <button class="btn text-center" style="background-color:#21791A; color:white" id="change_profile" disabled>Change Profile</button>
-                                                </div>
-                                            </div>
->>>>>>> a769b10b4907ffff9cfb7d77d7b6b4120e9aaabf
                                         </div>
                                     </div>
+
                                     <div class="col-9">
-                                                <h4 style="text-align: center; color:black">Profile Information</h4>
-                                            <div class="card-body">
-                                                <center>
-                                                    <div class="row ">
-                                                        <div class="form-group col-6 text-center">
-<<<<<<< HEAD
-
-                                                            <span style="font-size: 30px">{{ Auth::user()->name }}</span>
-                                                            <hr>
-                                                            <label for="name">Name</label>
-
-                                                        </div>
-                                                        <div class="form-group col-6 ">
-
-                                                            <span style="font-size: 30px">{{ Auth::user()->email }}</span>
-                                                            <hr>
-
-                                                            <label for="name">Email Address</label>
-
-                                                        </div>
+                                        <h4 style="text-align: center; color:black">Profile Information</h4>
+                                        <div class="card-body">
+                                            <center>
+                                                <div class="row ">
+                                                    <div class="form-group col-6 text-center">
+                                                        <span
+                                                            style="font-size: 20px;color:black">{{ Auth::user()->name }}</span>
+                                                        <hr style="color: #21791A">
+                                                        <label for="name">Name</label>
                                                     </div>
-                                                    <div class="row ">
-
-                                                        <div class="form-group">
-
-                                                            <span
-                                                                style="font-size: 15px">{{ Auth::user()->password }}</span>
-                                                            <hr>
-
-                                                            <label for="password">Password</label>
-                                                        </div>
-
+                                                    <div class="form-group col-6 ">
+                                                        <span
+                                                            style="font-size: 20px; color:black">{{ Auth::user()->email }}</span>
+                                                        <hr style="color: #21791A">
+                                                        <label class="small mb-1" for="name">Email Address</label>
                                                     </div>
-                                                    <div class="row ">
-
-                                                        <div class="form-group">
-
-                                                            <button class="btn btn-primary w-50">Edit Info</button>
-                                                        </div>
-
-=======
-                                                            <span style="font-size: 20px;color:black">{{ Auth::user()->name }}</span>
-                                                            <hr style="color: #21791A">
-                                                            <label for="name">Name</label>
-                                                        </div>
-                                                        <div class="form-group col-6 ">
-                                                            <span style="font-size: 20px; color:black">{{ Auth::user()->email }}</span>
-                                                            <hr style="color: #21791A">
-                                                            <label class="small mb-1" for="name">Email Address</label>
-                                                        </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="form-group">
+                                                        <span
+                                                            style="font-size: 15px; color:black">{{ Auth::user()->password }}</span>
+                                                        <hr style="color: #21791A">
+                                                        <label for="password">Password</label>
                                                     </div>
-                                                    <div class="row">
-                                                        <div class="form-group">
-                                                            <span
-                                                                style="font-size: 15px; color:black">{{ Auth::user()->password }}</span>
-                                                            <hr style="color: #21791A">
-                                                            <label for="password">Password</label>
-                                                        </div>
+                                                </div>
+                                                <div class="row ">
+                                                    <div class="form-group">
+                                                        <button class="btn w-50"
+                                                            style="background-color:#21791A; color:white">Edit
+                                                            Information</button>
                                                     </div>
-                                                    <div class="row ">
-                                                        <div class="form-group">
-                                                            <button class="btn w-50" style="background-color:#21791A; color:white">Edit Information</button>
-                                                        </div>
-
-                                                    </div>
-                                                </center>
-                                            </div>
+                                                </div>
+                                            </center>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
