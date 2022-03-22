53e5cc44b51101e56e5449c3ac58a8507c22bd17
Jan_Patrick Tancinco <janpatricktancinco@gmail.com>
2022-03-22 21:11:23 +0800

    good na ni

--- app/Http/Controllers/Admin/AddResortController.php
+++ app/Http/Controllers/Admin/AddResortController.php
@@ -35,18 +35,12 @@ class AddResortController extends Controller
         $resort_name = $request->input( 'resort_name' );   
 
         $resorts = Resort::where( 'resort_name', '=', $resort_name )->first();
-        if(empty($resorts->resort_name)){
         
+        if(empty($resorts->resort_name)){   
             $resort_description = $request->input( 'resort_description' );
             $path = 'data:image/' .  pathinfo($request->image, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents($request->image));
-    
-            $save = new Resort;
-    
-            $save->resort_description = $resort_description;
-            $save->resort_name = $resort_name;
-            $save->imagePath = $path;
 
-            $save->save();
+            Resort::insert(['imagePath'=>$path, 'resort_name' => $resort_name, 'resort_description' => $resort_description,] );
     
             return redirect( 'add_resort' )->with( 'status', 'Resort Successfully Added!' );
     
